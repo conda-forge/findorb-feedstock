5871525dadd5ffc475872d2dce7252b6da445a2c
Mario Juric <mjuric@astro.washington.edu>
2025-11-05 07:52:35 -0800

    Fix build

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,14 +7,16 @@ for s in lunar jpl_eph sat_code find_orb; do
 	(cd sources/$s && make clean)
 done
 
-CURSES_LIB=$([[ "$(uname)" == "Darwin" ]] && echo "-lncurses" || echo "-lncursesw")
+# makefile in jpl_eph uses CPP for the C++ compiler.  This collides with the
+# C preprocessor on Unix-y OS-es. Let's replace it with CXX
+sed -i.bak 's/\bCPP\b/CXX/g' sources/lunar/makefile && rm -f sources/lunar/makefile.bak
 
 # build all
 ( cd sources/lunar    && make                          && make install )
 ( cd sources/jpl_eph  && make libjpl.a                 && make install )
 ( cd sources/lunar    && make integrat                 && make install ) # must be a separate step as it depends on jpl_eph
 ( cd sources/sat_code && make sat_id                   && make install )
-( cd sources/find_orb && make CURSES_LIB="$CURSES_LIB" && make install )
+( cd sources/find_orb && make CURSES_LIB="-lncursesw"  && make install )
 
 # clean up the libraries and headers we won't distribute
 rm -f $PREFIX/lib/{libjpl,liblunar,libsatell}.a
